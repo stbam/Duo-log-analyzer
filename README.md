283240538cc66c069a4de7f31b219fecc3850d74
stbam <55661516+stbam@users.noreply.github.com>
2025-08-06 13:35:27 -0400

    Part 1 gets all logs

---
+++ README.md
@@ -0,0 +1,11 @@
+Project goal:
+- take duo security logs from duo application
+- use tensorflow to identify frauds that are serious after training on X amount of data.
+-before doing anything contact use using text or ai voice
+- take action (lock out,dismiss,mark suspicious etc.)
+
+
+goal of the project is to simplify the triage through Duo's offered api services
+
+
+
---
+++ init.py
@@ -0,0 +1,48 @@
+from duo_client.admin import Admin
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+admin_api = Admin(
+    ikey=os.getenv("DUO_IKEY"),  # integration key
+    skey=os.getenv("DUO_SKEY") ,       # secret key
+    host=os.getenv("DUO_HOST")
+)
+
+#last_seen= 0 
+
+try:
+    with open("last_seen.txt", "r") as f:
+        last_seen = int(f.read())
+except FileNotFoundError:
+    last_seen = 0
+
+
+while True:
+    print("Starting log fetch cycle...")
+    #print(dir(admin_api)) print lets me see all the commands available for duo logs 
+    try:
+        with open("last_seen.txt", "w") as f:
+            f.write(str(last_seen))
+        logs = admin_api.get_authentication_log(mintime=last_seen+1)
+
+        #for log in logs:
+        #    print(log)
+
+        for log in logs:
+            timestamp = log.get("timestamp")
+            if timestamp> last_seen:
+                last_seen = timestamp 
+                print("User:", log.get("username"))
+                print("Timestamp:", log.get("timestamp"))
+                print("Result:", log.get("result"))
+                print("IP:", log.get("ip"))
+                print("Device:", log.get("device"))
+                print("-" * 40)
+            print("time passed")
+        time.sleep(60)
+    except Exception as e:
+        print("error fetching logs: ",e)
+        time.sleep(60)
+
